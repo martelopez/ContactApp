0c8aec88b83aaee8768c77ae2c810117e385c4f8
emartelopez <emartelopez@gmail.com>
2016-11-22 08:44:57 -0500

    Adición del DatePicker

--- app/src/main/java/com/martelopez/contactapp/DetailContact.java
+++ app/src/main/java/com/martelopez/contactapp/DetailContact.java
@@ -18,11 +18,13 @@ public class DetailContact extends AppCompatActivity {
         final Bundle parametros = getIntent().getExtras();
 
         String nombre       = parametros.getString(getResources().getString(R.string.pnombre));
+        String fecha        = parametros.getString(getResources().getString(R.string.pfecha));
         String telefono     = parametros.getString(getResources().getString(R.string.ptelefono));
         String correo       = parametros.getString(getResources().getString(R.string.pcorreo));
         String descripcion  = parametros.getString(getResources().getString(R.string.pdescripcion));
 
         final TextView tvNombre       = (TextView) findViewById(R.id.tvNombre);
+        final TextView tvFecha        = (TextView) findViewById(R.id.tvFecha);
         final TextView tvTelefono     = (TextView) findViewById(R.id.tvTelefono);
         final TextView tvCorreo       = (TextView) findViewById(R.id.tvCorreo);
         final TextView tvDescripcion  = (TextView) findViewById(R.id.tvDescripcion);
@@ -31,10 +33,10 @@ public class DetailContact extends AppCompatActivity {
 
         // Mostrar el texto enviado por el Activity
         tvNombre.setText(nombre);
+        tvFecha.setText(fecha);
         tvTelefono.setText(telefono);
         tvCorreo.setText(correo);
         tvDescripcion.setText(descripcion);
-
 
         btnEditar.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/martelopez/contactapp/MainActivity.java
+++ app/src/main/java/com/martelopez/contactapp/MainActivity.java
@@ -1,34 +1,61 @@
 package com.martelopez.contactapp;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
-    private TextInputLayout tilNombre, tilTelefono, tilCorreo, tilDescripcion;
-    private EditText etNombre, etTelefono, etCorreo, etDescripcion;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextInputLayout tilNombre, tilFecha, tilTelefono, tilCorreo, tilDescripcion;
+    private EditText etNombre, etFecha, etTelefono, etCorreo, etDescripcion;
 
     Button btnSiguiente;
+
+    private DatePickerDialog fechaDatePickerDialog;
+    private SimpleDateFormat dateFormatter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tilNombre = (TextInputLayout) findViewById(R.id.tilNombre);
-        tilTelefono = (TextInputLayout) findViewById(R.id.tilTelefono);
-        tilCorreo = (TextInputLayout) findViewById(R.id.tilCorreo);
-        tilDescripcion = (TextInputLayout) findViewById(R.id.tilDescripcion);
+        tilNombre       = (TextInputLayout) findViewById(R.id.tilNombre);
+        tilFecha        = (TextInputLayout) findViewById(R.id.tilFecha);
+        tilTelefono     = (TextInputLayout) findViewById(R.id.tilTelefono);
+        tilCorreo       = (TextInputLayout) findViewById(R.id.tilCorreo);
+        tilDescripcion  = (TextInputLayout) findViewById(R.id.tilDescripcion);
+
+        dateFormatter = new SimpleDateFormat("dd-MM-yy", Locale.getDefault());
 
         etNombre = (EditText) findViewById(R.id.etNombre);
+        etFecha  = (EditText) findViewById(R.id.etFecha);
         etTelefono = (EditText) findViewById(R.id.etTelefono);
         etCorreo = (EditText) findViewById(R.id.etCorreo);
         etDescripcion = (EditText) findViewById(R.id.etDescripcion);
+
+        etFecha.setInputType(InputType.TYPE_NULL);
+        etFecha.setOnClickListener(this);
+
+        Calendar newCalendar = Calendar.getInstance();
+        fechaDatePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener(){
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth){
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year,monthOfYear,dayOfMonth);
+                etFecha.setText(dateFormatter.format(newDate.getTime()));
+            }
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
 
         btnSiguiente = (Button) findViewById(R.id.btnSiguiente);
 
@@ -37,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, DetailContact.class);
                 intent.putExtra(getResources().getString(R.string.pnombre), etNombre.getText().toString());
+                intent.putExtra(getResources().getString(R.string.pfecha), etFecha.getText().toString());
                 intent.putExtra(getResources().getString(R.string.ptelefono), etTelefono.getText().toString());
                 intent.putExtra(getResources().getString(R.string.pcorreo), etCorreo.getText().toString());
                 intent.putExtra(getResources().getString(R.string.pdescripcion), etDescripcion.getText().toString());
@@ -44,5 +72,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        fechaDatePickerDialog.show();
     }
 }
